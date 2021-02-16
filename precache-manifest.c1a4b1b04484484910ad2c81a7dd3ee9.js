6034455fae43f2b944f355a8793c05c1fc7b08d0
basavarajnavalgund <basavarajnavalgund97@gmail.com>
2021-02-16 23:27:19 +0530

    updated

---
+++ precache-manifest.c1a4b1b04484484910ad2c81a7dd3ee9.js
@@ -0,0 +1,298 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f1ac4dc7fd50cd64ba487eeb20603ddf",
+    "url": "/basavarajnavalgund.github.io/index.html"
+  },
+  {
+    "revision": "084dcbd6a8df5db7e7c3",
+    "url": "/basavarajnavalgund.github.io/static/css/main.8b78b12a.chunk.css"
+  },
+  {
+    "revision": "357f18fcd1151e0366a7",
+    "url": "/basavarajnavalgund.github.io/static/js/2.6cd86f8e.chunk.js"
+  },
+  {
+    "revision": "084dcbd6a8df5db7e7c3",
+    "url": "/basavarajnavalgund.github.io/static/js/main.d07e3d28.chunk.js"
+  },
+  {
+    "revision": "2f3578ce3f10d85f5892",
+    "url": "/basavarajnavalgund.github.io/static/js/runtime-main.49cbd98f.js"
+  },
+  {
+    "revision": "21f233e19402cc4a66866a7f31191f0d",
+    "url": "/basavarajnavalgund.github.io/static/media/Agustina.21f233e1.woff"
+  },
+  {
+    "revision": "4457817ac2b9993c65e81aa05828fe9c",
+    "url": "/basavarajnavalgund.github.io/static/media/GoogleSans-Bold.4457817a.ttf"
+  },
+  {
+    "revision": "90773b6158663ab0fe78b32680733677",
+    "url": "/basavarajnavalgund.github.io/static/media/GoogleSans-BoldItalic.90773b61.ttf"
+  },
+  {
+    "revision": "0ecddcdeccb7761ce899aa9ad9f0ac3f",
+    "url": "/basavarajnavalgund.github.io/static/media/GoogleSans-Italic.0ecddcde.ttf"
+  },
+  {
+    "revision": "8d57e4014b18edef070d285746485115",
+    "url": "/basavarajnavalgund.github.io/static/media/GoogleSans-Medium.8d57e401.ttf"
+  },
+  {
+    "revision": "8fd3737925e83c87d78a13700ccf9a62",
+    "url": "/basavarajnavalgund.github.io/static/media/GoogleSans-MediumItalic.8fd37379.ttf"
+  },
+  {
+    "revision": "b5c77a6aed75cdad9489effd0d5ea411",
+    "url": "/basavarajnavalgund.github.io/static/media/GoogleSans-Regular.b5c77a6a.ttf"
+  },
+  {
+    "revision": "ee6539921d713482b8ccd4d0d23961bb",
+    "url": "/basavarajnavalgund.github.io/static/media/Montserrat-Regular.ee653992.ttf"
+  },
+  {
+    "revision": "3b021f8ffcf9f323ea72abc28382e234",
+    "url": "/basavarajnavalgund.github.io/static/media/address_image.3b021f8f.svg"
+  },
+  {
+    "revision": "52e0714e5ea7e3b96f50dbfb62de21d1",
+    "url": "/basavarajnavalgund.github.io/static/media/animated_logo.52e0714e.svg"
+  },
+  {
+    "revision": "b6b12c7d07e0569e771c8aedf81a7767",
+    "url": "/basavarajnavalgund.github.io/static/media/blogs_image.b6b12c7d.svg"
+  },
+  {
+    "revision": "7a113100d827cefe8fd39a1c330123e5",
+    "url": "/basavarajnavalgund.github.io/static/media/car.7a113100.svg"
+  },
+  {
+    "revision": "dfcb7a585547c0b125d208b980d6f57f",
+    "url": "/basavarajnavalgund.github.io/static/media/cloud_infrastructure.dfcb7a58.svg"
+  },
+  {
+    "revision": "d6d4086fb1cade58b3f786620e17c3ef",
+    "url": "/basavarajnavalgund.github.io/static/media/contactMail.d6d4086f.png"
+  },
+  {
+    "revision": "51cf7192327488da3d2a171280680c16",
+    "url": "/basavarajnavalgund.github.io/static/media/contactMailDark.51cf7192.svg"
+  },
+  {
+    "revision": "7ca3fc1d6ba178eafcbfa732501740ed",
+    "url": "/basavarajnavalgund.github.io/static/media/conti_logo.7ca3fc1d.png"
+  },
+  {
+    "revision": "2fc9da616c034a22a8c1efeef05ea536",
+    "url": "/basavarajnavalgund.github.io/static/media/conti_logo1.2fc9da61.png"
+  },
+  {
+    "revision": "434d2b012c3efc3da1ef90549a50f46b",
+    "url": "/basavarajnavalgund.github.io/static/media/data_science.434d2b01.svg"
+  },
+  {
+    "revision": "33fa25c9b4573f0edc671920707f2b8f",
+    "url": "/basavarajnavalgund.github.io/static/media/developerActivity.33fa25c9.svg"
+  },
+  {
+    "revision": "7345ee7cfde2d636c844faf458fe6afb",
+    "url": "/basavarajnavalgund.github.io/static/media/developer_activity.7345ee7c.svg"
+  },
+  {
+    "revision": "caed892b2a0b817a8e275ae29638edaf",
+    "url": "/basavarajnavalgund.github.io/static/media/education.caed892b.svg"
+  },
+  {
+    "revision": "85d6957f6a0d678d1ac3932e5be4fcb2",
+    "url": "/basavarajnavalgund.github.io/static/media/experience.85d6957f.svg"
+  },
+  {
+    "revision": "06147b6cd88c7346cecd1edd060cd5de",
+    "url": "/basavarajnavalgund.github.io/static/media/fa-brands-400.06147b6c.ttf"
+  },
+  {
+    "revision": "5063b105c7646c8043d58c5289f02cca",
+    "url": "/basavarajnavalgund.github.io/static/media/fa-brands-400.5063b105.eot"
+  },
+  {
+    "revision": "a9c4bb7348f42626454c988dbde1d0a0",
+    "url": "/basavarajnavalgund.github.io/static/media/fa-brands-400.a9c4bb73.svg"
+  },
+  {
+    "revision": "c5e0f14f88a828261ba01558ce2bf26f",
+    "url": "/basavarajnavalgund.github.io/static/media/fa-brands-400.c5e0f14f.woff"
+  },
+  {
+    "revision": "cccc9d29470e879e40eb70249d9a2705",
+    "url": "/basavarajnavalgund.github.io/static/media/fa-brands-400.cccc9d29.woff2"
+  },
+  {
+    "revision": "65b286af947c0d982ca01b40e1fcab38",
+    "url": "/basavarajnavalgund.github.io/static/media/fa-regular-400.65b286af.ttf"
+  },
+  {
+    "revision": "7b9568e6389b1f8ae0902cd39665fc1e",
+    "url": "/basavarajnavalgund.github.io/static/media/fa-regular-400.7b9568e6.svg"
+  },
+  {
+    "revision": "c1a866ec0e04a5e1915b41fcf261457c",
+    "url": "/basavarajnavalgund.github.io/static/media/fa-regular-400.c1a866ec.eot"
+  },
+  {
+    "revision": "c4f508e7c4f01a9eeba7f08155cde04e",
+    "url": "/basavarajnavalgund.github.io/static/media/fa-regular-400.c4f508e7.woff"
+  },
+  {
+    "revision": "f5f2566b93e89391da4db79462b8078b",
+    "url": "/basavarajnavalgund.github.io/static/media/fa-regular-400.f5f2566b.woff2"
+  },
+  {
+    "revision": "0bff33a5fd7ec390235476b4859747a0",
+    "url": "/basavarajnavalgund.github.io/static/media/fa-solid-900.0bff33a5.ttf"
+  },
+  {
+    "revision": "333bae208dc363746961b234ff6c2500",
+    "url": "/basavarajnavalgund.github.io/static/media/fa-solid-900.333bae20.woff"
+  },
+  {
+    "revision": "44d537ab79f921fde5a28b2c1636f397",
+    "url": "/basavarajnavalgund.github.io/static/media/fa-solid-900.44d537ab.woff2"
+  },
+  {
+    "revision": "8e4a6dcc692b3887f9f542cd6894d6d4",
+    "url": "/basavarajnavalgund.github.io/static/media/fa-solid-900.8e4a6dcc.eot"
+  },
+  {
+    "revision": "c2801fb415f03c7b170934769d7b5397",
+    "url": "/basavarajnavalgund.github.io/static/media/fa-solid-900.c2801fb4.svg"
+  },
+  {
+    "revision": "a73310ea0ad8aebedb65b3106024427a",
+    "url": "/basavarajnavalgund.github.io/static/media/feelingProud.a73310ea.svg"
+  },
+  {
+    "revision": "589bec7d632ecff9672dc4da97897bbd",
+    "url": "/basavarajnavalgund.github.io/static/media/fullstack.589bec7d.svg"
+  },
+  {
+    "revision": "ba18401b15f40a463898b706cc6ff08c",
+    "url": "/basavarajnavalgund.github.io/static/media/gcp_logo.ba18401b.png"
+  },
+  {
+    "revision": "e64de169a4222787bc49f3706282ef96",
+    "url": "/basavarajnavalgund.github.io/static/media/github_logo.e64de169.png"
+  },
+  {
+    "revision": "0c8a4c114ad00f7854e91550379b9b91",
+    "url": "/basavarajnavalgund.github.io/static/media/googleAssistant.0c8a4c11.jpg"
+  },
+  {
+    "revision": "c24ba79111a90758a3a472a906ab2901",
+    "url": "/basavarajnavalgund.github.io/static/media/googleAssistant.c24ba791.svg"
+  },
+  {
+    "revision": "2ee7025ee8a3dfd9c701b2fdd36c7905",
+    "url": "/basavarajnavalgund.github.io/static/media/interests.2ee7025e.svg"
+  },
+  {
+    "revision": "acefbb9853f070fbca23dd0de6ed8437",
+    "url": "/basavarajnavalgund.github.io/static/media/interests2.acefbb98.svg"
+  },
+  {
+    "revision": "692a449bfc7247973c6a1fe357d5eef9",
+    "url": "/basavarajnavalgund.github.io/static/media/interests3.692a449b.svg"
+  },
+  {
+    "revision": "0e09dd743d977cd0be3141e41a7c57e2",
+    "url": "/basavarajnavalgund.github.io/static/media/interests4.0e09dd74.svg"
+  },
+  {
+    "revision": "b05ae5b575a7e1125610944dc152c35f",
+    "url": "/basavarajnavalgund.github.io/static/media/jsFramework.b05ae5b5.svg"
+  },
+  {
+    "revision": "8a0b4392ec1eb72b1645b6784c3a08fb",
+    "url": "/basavarajnavalgund.github.io/static/media/klelogo.8a0b4392.png"
+  },
+  {
+    "revision": "52be31441c8cf8a8a16bd7a228d428bf",
+    "url": "/basavarajnavalgund.github.io/static/media/manOnTable.52be3144.svg"
+  },
+  {
+    "revision": "61179bf2dd896642628e38d773d7b8fb",
+    "url": "/basavarajnavalgund.github.io/static/media/mando_logo.61179bf2.png"
+  },
+  {
+    "revision": "66271f7ae972df3810ad8e52f46dc15d",
+    "url": "/basavarajnavalgund.github.io/static/media/myimg.66271f7a.png"
+  },
+  {
+    "revision": "c1a95f7335cbfb730b937e19e8830a53",
+    "url": "/basavarajnavalgund.github.io/static/media/programmer.c1a95f73.svg"
+  },
+  {
+    "revision": "f7964c6bf45e77547f1e5aae1e1dede0",
+    "url": "/basavarajnavalgund.github.io/static/media/projects_image.f7964c6b.svg"
+  },
+  {
+    "revision": "a84ad273d14ac7257d7e537ad1b2b19d",
+    "url": "/basavarajnavalgund.github.io/static/media/robot.a84ad273.svg"
+  },
+  {
+    "revision": "91d5f28869e3eeaaec1ee45f2c42d09d",
+    "url": "/basavarajnavalgund.github.io/static/media/skill.91d5f288.svg"
+  },
+  {
+    "revision": "cf72b4f8f0b7219f425f4b51158c6449",
+    "url": "/basavarajnavalgund.github.io/static/media/skill2.cf72b4f8.svg"
+  },
+  {
+    "revision": "0288c5ec7a87ea73f8d14b12fc4bc94e",
+    "url": "/basavarajnavalgund.github.io/static/media/talksCardBack.0288c5ec.svg"
+  },
+  {
+    "revision": "b48e86c9a956d3a3a32ca4a5ae05ddc7",
+    "url": "/basavarajnavalgund.github.io/static/media/talksCardBackAlt.b48e86c9.svg"
+  },
+  {
+    "revision": "9a6ac0f83088646a6c01d35715bbf78b",
+    "url": "/basavarajnavalgund.github.io/static/media/technoyantra_logo.9a6ac0f8.png"
+  },
+  {
+    "revision": "ed3a6588eadb03cc87879926caca080d",
+    "url": "/basavarajnavalgund.github.io/static/media/udacity_logo.ed3a6588.png"
+  },
+  {
+    "revision": "67a6a11c47edafb87cf24da66afe3dca",
+    "url": "/basavarajnavalgund.github.io/static/media/ui_ux_design.67a6a11c.svg"
+  },
+  {
+    "revision": "e871c0214955301e1f851413113eceb1",
+    "url": "/basavarajnavalgund.github.io/static/media/undraw_Firmware_jw6u.e871c021.svg"
+  },
+  {
+    "revision": "63495aba8b10f5f4c4b49dc90d7b68c8",
+    "url": "/basavarajnavalgund.github.io/static/media/undraw_coding_6mjf.63495aba.svg"
+  },
+  {
+    "revision": "f52759a387412e8da5df27bbf24fe3b4",
+    "url": "/basavarajnavalgund.github.io/static/media/undraw_data_processing_yrrv.f52759a3.svg"
+  },
+  {
+    "revision": "bdcbc871b757b5c7f933aaa1a62e7035",
+    "url": "/basavarajnavalgund.github.io/static/media/undraw_dev_productivity_umsq.bdcbc871.svg"
+  },
+  {
+    "revision": "4284e507718068890decf7329e1f466a",
+    "url": "/basavarajnavalgund.github.io/static/media/undraw_manage_chats_ylx0.4284e507.svg"
+  },
+  {
+    "revision": "b9cf7af4239b79225fc0119b758d1b9a",
+    "url": "/basavarajnavalgund.github.io/static/media/undraw_programming_2svr.b9cf7af4.svg"
+  },
+  {
+    "revision": "37814f7f919081b2fd632f8318a89f02",
+    "url": "/basavarajnavalgund.github.io/static/media/undraw_web_developer_p3e5.37814f7f.svg"
+  }
+]);
